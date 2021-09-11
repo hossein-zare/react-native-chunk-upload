1505a3ec1820c9f4e4e3a5a89382399f73fb7fa6
Hossein Zare <zarehossein.hz1997@gmail.com>
2021-09-11 14:02:36 +0430

    Typedef improvement.

--- index.d.ts
+++ index.d.ts
@@ -5,44 +5,42 @@ export = ChunkUpload;
 declare class ChunkUpload {
   constructor(props: ChunkUpload.Props);
 
-  digIn(Event: ChunkUpload.Event): void
+  digIn(Event: ChunkUpload.Event): void;
 }
 
 declare namespace ChunkUpload {
     export interface Props {
-        path: string,
-        size: number,
-        fileName: string,
-        fileSize: number
+        path: string;
+        size: number;
+        fileName: string;
+        fileSize: number;
     }
 
-    export interface Event {
-        (
-            file: File,
-            next: () => void,
-            retry: () => void,
-            unlink: (path: string) => void
-        ): void
-    }
+    export type Event = (
+        file: File,
+        next: () => void,
+        retry: () => void,
+        unlink: (path: string) => void
+    ) => void;
 
     export interface File {
-        path: string,
-        headers: Header,
-        blob: Blob
+        path: string;
+        headers: Header;
+        blob: Blob;
     }
 
     export interface Header {
-        "x-chunk-number": number,
-        "x-chunk-total-number": number,
-        "x-chunk-size": number,
-        "x-file-name": string,
-        "x-file-size": number,
-        "x-file-identity": string
+        "x-chunk-number": number;
+        "x-chunk-total-number": number;
+        "x-chunk-size": number;
+        "x-file-name": string;
+        "x-file-size": number;
+        "x-file-identity": string;
     }
 
     export interface Blob {
-        name: string,
-        type: string,
-        uri: string
+        name: string;
+        type: string;
+        uri: string;
     }
 }
